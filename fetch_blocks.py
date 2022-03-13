1d00ef8ae8bdeadeaab72f2669af7dd7f041e015
Omer Drori <odrori574@gmail.com>
2022-03-13 19:51:37 -0400

    cleaning up code

--- fetch_blocks.py
+++ fetch_blocks.py
@@ -6,71 +6,53 @@ import pandas as pd
 import gzip
 import shutil
 from urllib import request
+import os
 
 # Using https://gz.blockchair.com/bitcoin/blocks/
-URL_BASE = f"https://chain.api.btc.com/v3/block"
+URL_BASE = f"https://gz.blockchair.com/bitcoin/outputs/blockchair_bitcoin_outputs_"
 
-def query_btc(endpoint):
-    response = requests.get(URL_BASE+endpoint)
-    print(response)
-    try:
-        parsed_response = response.json()
-    except Exception as e:
-        print(f"Error parsing json response: {e}")
-        return {}
-    if parsed_response["err_no"] == 0:
-        print(parsed_response)
-        return parsed_response
-    else:
-        print(f"Error: " + str(parsed_response["err_no"]))    
-    return {}
-
-
-start_date = "2009/01/02"
+start_date = "2009/01/02" # date of bitcoin inception
 start_dt = datetime.strptime(start_date, "%Y/%m/%d")
-for i in range(365):
-    start_dt += timedelta(days=1)
-    endpoint = "https://gz.blockchair.com/bitcoin/outputs/blockchair_bitcoin_outputs_" + start_dt.strftime("%Y%m%d") +".tsv.gz?key=A___MmrGR8YFGGAeYAfhesqNsQlrPmMD"
-    resp = requests.get(endpoint)
-    start_dt += timedelta(days=1)
-    try:
-        parsed_response = resp.json()
-    except Exception as e:
-        print(f"Error: {e}")
-        continue
-    fname = start_dt.strftime("%Y%m%d") + '.csv'
-    resp = requests.get(endpoint, verify=False)
-    try:
-        with open("temp.tsv.gz", 'wb') as f:
-            f.write(resp.content)
-            print("Wrote temp.tsv.gz")
-        with gzip.open("temp.tsv.gz", 'rb') as f_in:
-            with open(fname, 'wb') as f_out:
-                shutil.copyfileobj(f_in, f_out)
-    except Exception as e:
-        print(f"Error: {e}")
-        continue
 
-start_date = "2009/01/02"
-start_dt = datetime.strptime(start_date, "%Y/%m/%d")
-prev = datetime.now()
-block_counter = 0
-for i in range(365):
-    start_dt += timedelta(days=1)
-    start_dt += timedelta(days=1)
-    fname = start_dt.strftime("%Y%m%d") + '.csv'
-    with open(fname, "rb") as f:
-        try: 
-            df = pd.read_csv(fname,delimiter="\t")
-            print(df)
+def download_files(start_dt):
+    for _ in range(365):
+        start_dt += timedelta(days=1)
+        endpoint = URL_BASE + start_dt.strftime("%Y%m%d") +".tsv.gz?key=" + os.environ("APIKEY")
+        fname = start_dt.strftime("%Y%m%d") + '.csv'
+        resp = requests.get(endpoint, verify=False)
+        try:
+            with open("temp.tsv.gz", 'wb') as f:
+                f.write(resp.content)
+            with gzip.open("temp.tsv.gz", 'rb') as f_in:
+                with open(fname, 'wb') as f_out:
+                    shutil.copyfileobj(f_in, f_out)
         except Exception as e:
             print(f"Error: {e}")
             continue
-        for ind, row in df.iterrows():
-            print(row.keys())
-            tdelta = datetime.strptime(row["time"], "%Y-%m-%d %H:%M:%S") - prev
-            print(tdelta)
-            if tdelta >= timedelta(hours=2):
-                block_counter += 1
-            print("Block counter: " + str(block_counter) + "\nFound block mined " + str(tdelta) + " after previous")
-            prev = row["time"]
+
+
+def count_block_intervals(start_dt):
+    prev = datetime.now()
+    block_counter = -1 # first block will count as >2 hours
+    for _ in range(365):
+        start_dt += timedelta(days=1)
+        fname = start_dt.strftime("%Y%m%d") + '.csv'
+        with open(fname, "rb") as f:
+            try: 
+                df = pd.read_csv(fname,delimiter="\t")
+                print(df)
+            except Exception as e:
+                print(f"Error: {e}")
+                continue
+            for _, row in df.iterrows():
+                tdelta = datetime.strptime(row["time"], "%Y-%m-%d %H:%M:%S") - prev
+                if tdelta >= timedelta(hours=2):
+                    block_counter += 1
+                print("Block counter: " + str(block_counter) + "\nFound block mined " + str(tdelta) + " after previous")
+                prev = row["time"]
+    return block_counter
+
+download_files(start_dt)
+
+print(" -- Final count -- ")
+print(count_block_intervals(start_dt))
