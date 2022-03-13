b60f25f47143732dd1b690df03e4a42a4f0e344f
Omer Drori <odrori574@gmail.com>
2022-03-13 19:42:29 -0400

    final solution

--- fetch_blocks.py
+++ fetch_blocks.py
@@ -1,8 +1,13 @@
 import requests
 import datetime
+from datetime import datetime, timedelta
 import time
+import pandas as pd
+import gzip
+import shutil
+from urllib import request
 
-# Using https://btc.com/btc/adapter?type=api-doc
+# Using https://gz.blockchair.com/bitcoin/blocks/
 URL_BASE = f"https://chain.api.btc.com/v3/block"
 
 def query_btc(endpoint):
@@ -20,22 +25,52 @@ def query_btc(endpoint):
         print(f"Error: " + str(parsed_response["err_no"]))    
     return {}
 
-prev = datetime.datetime.now()
-block_counter = 0
-for i in range(1,100800):
-    # print(f"Processing block: {current_height-i}")
-    print(f"Processing block: {i}")
-    time.sleep(10) # sleep to prevent rate limiting
-    # response = query_btc(f"/{current_height-i}") 
-    response = query_btc(f"/{i}") 
-    if not response:
-        print("error from btc api: Empty response")
+
+start_date = "2009/01/02"
+start_dt = datetime.strptime(start_date, "%Y/%m/%d")
+for i in range(365):
+    start_dt += timedelta(days=1)
+    endpoint = "https://gz.blockchair.com/bitcoin/outputs/blockchair_bitcoin_outputs_" + start_dt.strftime("%Y%m%d") +".tsv.gz?key=A___MmrGR8YFGGAeYAfhesqNsQlrPmMD"
+    resp = requests.get(endpoint)
+    start_dt += timedelta(days=1)
+    try:
+        parsed_response = resp.json()
+    except Exception as e:
+        print(f"Error: {e}")
         continue
-    curr_ts = datetime.datetime.fromtimestamp(response["data"]["timestamp"])
-    # tdelta = prev - curr_ts
-    tdelta = curr_ts - prev
-    if tdelta >= datetime.timedelta(hours=2):
-        block_counter += 1
-    print("Block counter: " + str(block_counter) + "\nFound block mined " + str(tdelta) + " after previous")
-    prev = curr_ts
-print(block_counter)
+    fname = start_dt.strftime("%Y%m%d") + '.csv'
+    resp = requests.get(endpoint, verify=False)
+    try:
+        with open("temp.tsv.gz", 'wb') as f:
+            f.write(resp.content)
+            print("Wrote temp.tsv.gz")
+        with gzip.open("temp.tsv.gz", 'rb') as f_in:
+            with open(fname, 'wb') as f_out:
+                shutil.copyfileobj(f_in, f_out)
+    except Exception as e:
+        print(f"Error: {e}")
+        continue
+
+start_date = "2009/01/02"
+start_dt = datetime.strptime(start_date, "%Y/%m/%d")
+prev = datetime.now()
+block_counter = 0
+for i in range(365):
+    start_dt += timedelta(days=1)
+    start_dt += timedelta(days=1)
+    fname = start_dt.strftime("%Y%m%d") + '.csv'
+    with open(fname, "rb") as f:
+        try: 
+            df = pd.read_csv(fname,delimiter="\t")
+            print(df)
+        except Exception as e:
+            print(f"Error: {e}")
+            continue
+        for ind, row in df.iterrows():
+            print(row.keys())
+            tdelta = datetime.strptime(row["time"], "%Y-%m-%d %H:%M:%S") - prev
+            print(tdelta)
+            if tdelta >= timedelta(hours=2):
+                block_counter += 1
+            print("Block counter: " + str(block_counter) + "\nFound block mined " + str(tdelta) + " after previous")
+            prev = row["time"]
